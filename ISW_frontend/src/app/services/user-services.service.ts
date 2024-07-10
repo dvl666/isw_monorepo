7da6a38e0a12390205deff0faf02d3097da8664b
dvl666 <kamagaspar@gmail.com>
2024-07-10 03:33:48 -0400

    add register page

--- ISW_frontend/src/app/services/user-services.service.ts
+++ ISW_frontend/src/app/services/user-services.service.ts
@@ -13,6 +13,14 @@ export class UserServicesService {
 
   private apiUrl = 'http://localhost:3000'
 
+  registerStudent(email: string,name: string , password: string, contactNumber: number): Observable<any> {
+    return this.http.post(this.apiUrl + '/user/create-student', { email: email, password: password, name: name, contactNumber: contactNumber })
+  }
+
+  registerTeacher(email: string,name: string , password: string, specialty: string, contactNumber: number): Observable<any> {
+    return this.http.post(this.apiUrl + '/user/create-teacher', { email: email, password: password, name: name, specialty: specialty, contactNumber: contactNumber })
+  }
+
   logIn(email: string, password: string): Observable<any> {
     return this.http.post(this.apiUrl + '/user/login', { email: email, password: password })
   }
@@ -23,7 +31,7 @@ export class UserServicesService {
 
   acceptTeacherRequest(teacherId: string): Observable<any> {
     console.log(teacherId)
-    return this.http.post(this.apiUrl+'/user/accept-request/', {id: teacherId })
+    return this.http.post(this.apiUrl +'/user/accept-request/', {id: teacherId })
   }
 
 }
